83bd46f3a09befa6bfea309453a57b3963734004
Kwanzu-J <jkwanzu105@gmail.com>
2023-06-12 14:28:21 -0700

    100-argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -23,7 +23,7 @@ char *argstostr(int ac, char **av)
 	}
 	size++;
 /**
- *allocate memory for total number of chars and a new line for each word
+ *strDup - allocate memory for number of chars & newline for each word
  */
 	strDup = malloc(sizeof(char) * size);
 	if (strDup == NULL)
